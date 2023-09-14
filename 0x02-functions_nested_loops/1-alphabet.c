b3d353761676617f7bc507239bfc38a71dff7f56
emma-adu-saah <aduemma07@gmail.com>
2023-09-13 22:57:40 -0700

    Add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_alphabet - Prints the alphabet in lowercase
+ *
+ *Return: Returns 0
+ **/
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
