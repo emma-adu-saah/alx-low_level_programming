2d26f2b7f9cb9e9c6a394a7f793016484c7e3aa4
emma-adu-saah <aduemma07@gmail.com>
2023-09-14 16:17:37 -0700

    Add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - Print the numbers from 0 - 9
+ *
+ *Return: Nothing
+ **/
+void print_numbers(void)
+{
+int num = 48;
+while (num <= 57)
+{
+_putchar(num);
+num++;
+}
+_putchar('\n');
+}
