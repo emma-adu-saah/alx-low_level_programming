ebb960ae9ea83734aa663f6b433a62a0eff25754
emma-adu-saah <aduemma07@gmail.com>
2023-09-13 23:26:57 -0700

    Add 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_alphabet_x10 - Prints the alphabets 10 times
+ *
+ *Return: Returns 0
+ **/
+void print_alphabet_x10(void)
+{
+int i;
+char letter;
+for (i = 0; i <= 10; i++)
+{
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+}
