4ff81561e9535aff01ce825a44dd5d0da5730a21
emma-adu-saah <aduemma07@gmail.com>
2023-09-14 18:16:08 -0700

    Correct 7-print_diagonal 1

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,14 @@
 void print_diagonal(int n)
 {
 int i;
-for (i = 0; i <= n; i++)
+for (i = 0; i <= n - 1; i++)
 {
 if (n <= 0)
 {
 _putchar('\n');
 }
-_putchar(92);
+_putchar(32);
 }
+_putchar(92);
 _putchar('\n');
 }
