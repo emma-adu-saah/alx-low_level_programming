54c2cd1170d75eeec7a9371f591fe72fc2706270
emma-adu-saah <aduemma07@gmail.com>
2023-09-20 03:59:35 -0700

    6-puts2.c conform to Betty-style coding

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,10 +6,10 @@
  **/
 void puts2(char *str)
 {
-  int i;
-  for (i = 0; str[i] !='\0'; i = i + 2)
-    {
-      _putchar(str[i]);
-    }
-  _putchar('\n');
+int i;
+for (i = 0; str[i] !='\0'; i = i + 2)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
