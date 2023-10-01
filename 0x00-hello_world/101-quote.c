156ccd8232a7d558bd82bd78cc70d22f54343f96
emma-adu-saah <aduemma07@gmail.com>
2023-09-30 17:08:51 -0700

    101-quote.c conform to betty-style coding and documentation

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *Return - Returns 1
+ *Return: Returns 1
  **/
-int main ()
+int main(void)
 {
 char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 int i;
