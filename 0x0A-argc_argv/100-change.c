4b0ccaffafdfbc2bc321881ca149317845e84bb2
emma-adu-saah <aduemma07@gmail.com>
2023-10-21 16:19:08 -0700

    Change corrected

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,12 +9,12 @@ int main(int argc, char **argv)
 {
 int i, cents, num_coins, coin_count;
 int coin_values[5] = {25, 10, 5, 2, 1};
-cents = atoi(argv[1]);
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
+cents = atoi(argv[1]);
 if (cents < 0)
 {
 printf("0\n");
