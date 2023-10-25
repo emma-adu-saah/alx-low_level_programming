7c6841107ecc2bbf65f209bd2134d75d63327907
emma-adu-saah <aduemma07@gmail.com>
2023-10-25 14:57:57 -0700

    Correct 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,29 @@
-/*******************************************************************************
-** Function: _sqrt_recursion()
-** Description:
-**   Returns the natural square root of the given number, or -1 if the number
-**   does not have a natural square root.
-**
-** Parameters:
-**   n: The number to find the square root of.
-**
-** Returns:
-**   The natural square root of n, or -1 if n does not have a natural square root.
-**
-** Notes:
-**   This function uses a recursive algorithm to find the square root.
-**
-*******************************************************************************/
+#include "main.h"
+#include <stdio.h>
+/**
+*_sqrt_recursion - Calculates the natural square root of a number
+*@n: Input integer
+*Return: An Integer
+**/
 int _sqrt_recursion(int n)
 {
-    /* Check if n is a perfect square. If so, return its square root. */
-    if (n >= 0 && n * n == n) {
-        return n;
-    }
+	if (n < 0)
+		return (-1);
+	return (find_sqrt(n, 0));
+}
 
-    /* If n is negative, it does not have a natural square root. */
-    if (n < 0) {
-        return -1;
-    }
-
-    /* Otherwise, recursively search for the square root of n. */
-    int mid = n / 2;
-
-    /* If mid squared is less than n, the square root is greater than mid. */
-    if (mid * mid < n) {
-        return _sqrt_recursion(n - mid * mid);
-    }
-
-    /* Otherwise, the square root is less than mid. */
-    else {
-        return _sqrt_recursion(mid);
-    }
+/**
+*find_sqrt - recurses to find the natural square root
+*of a number
+*@n: number to calculate the sqaure root of
+*@i: iterator
+*Return: the resulting square root
+**/
+int find_sqrt(int n, int i)
+{
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (find_sqrt(n, i + 1));
 }
