7ac8cb3ba95d251d73a1134ea7bfadb24498ea1b
emma-adu-saah <aduemma07@gmail.com>
2023-10-25 15:01:26 -0700

    Correct 5-sqrt_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,14 @@
-int _putchar(char c);
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _sqrt_recursion_helper(int n, int guess);
 int is_prime_number(int n);
-
+int find_sqrt(int n, int i);
+int actual_prime(int n, int i);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
