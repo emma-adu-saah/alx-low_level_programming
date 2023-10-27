647b80429e32aeade7d5ccc029e1d67dc6391281
emma-adu-saah <aduemma07@gmail.com>
2023-10-27 22:43:34 +0000

    Correct 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,9 +6,7 @@
  **/
 void print_binary(unsigned long int n)
 {
-	char binary[32];
 	unsigned long int i = 0;
-	int j;
 
 	if (n == 0)
 	{
@@ -19,17 +17,13 @@ void print_binary(unsigned long int n)
 	{
 		if (n & 1)
 		{
-			binary[i] = '1';
+			_putchar('1');
 		}
 		else
 		{
-			binary[i] = '0';
+			_putchar('0');
 		}
 		n = n >> 1;
 		i++;
-	}
-	for (j = i - 1; j >= 0; j--)
-	{
-		_putchar(binary[j]);
 	}
 }
