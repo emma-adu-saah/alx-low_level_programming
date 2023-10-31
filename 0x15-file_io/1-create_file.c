50873dd0afe144f4abc5ec6995aafcfe33e9015b
emma-adu-saah <aduemma07@gmail.com>
2023-10-31 01:52:56 +0000

    Add 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *create_file - Creates a file
+ *@filename: Name of the file to be created
+ *@text_content: Content to write to the file
+ *Return: 1 on success, -1 on failure
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+	size_t text_len = sizeof(text_content);
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	bytes_written = write(fd, text_content, text_len);
+	if (bytes_written == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
